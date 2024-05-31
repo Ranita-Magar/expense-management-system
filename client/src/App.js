9ea2e296829411d0efed0e1d5698b51e14975753
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-05-31 12:01:30 +0545

    Reactjs Routing & Layout in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">Hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
